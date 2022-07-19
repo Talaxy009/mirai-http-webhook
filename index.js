b610f507b2e2c08581adbfd9c42de31076f9f5a1
Taozc <x009ba1@gmail.com>
2022-07-19 20:09:11 +0800

    🏗️ 使用 express 并优化代码结构

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const Mirai = require('node-mirai-sdk');
-const http = require('http');
-const {URL} = require('url');
+const express = require('express');
+const bodyParser = require('body-parser');
 const config = require('./config.json');
 
 const bot = new Mirai(config.mirai);
+const server = express();
 const {Plain, Image} = Mirai.MessageComponent;
 const getTime = () => new Date().toLocaleString();
+const ckeckToken = (token = '') =>
+	config.webhook.token.length && config.webhook.token !== token;
+const responseText = [
+	'success', // 成功
+	'token invalid', // token 失效
+	'unsupported method', // 不支持的请求方式
+	'server error', // 服务器错误
+];
 
 bot.onSignal('authed', () => {
-	console.log(`${getTime()} 通过: ${bot.sessionKey} 认证中···`);
+	console.log(`${getTime()} 通过：${bot.sessionKey} 认证中···`);
 	bot.verify();
 });
 
 bot.onSignal('verified', () => {
 	sendRawMessage('Webhook 已启用', null);
-	console.log(`${getTime()} 通过: ${bot.sessionKey} 认证成功!\n`);
+	console.log(`${getTime()} 通过：${bot.sessionKey} 认证成功！\n`);
 });
 
+/**
+ * 发送 QQ 消息
+ * @param {string} text 文本消息
+ * @param {string} image 图片消息（图片 URL）
+ */
 function sendRawMessage(text, image) {
 	let messageChain = [];
 	if (text && text.length) {
@@ -32,55 +46,57 @@ function sendRawMessage(text, image) {
 	}
 }
 
-const server = http.createServer((req, res) => {
-	const {searchParams} = new URL(req.url, 'http://localhost/');
-	let token = searchParams.get('token');
-	let text, image;
-	if (req.method === 'GET') {
-		text = searchParams.get('text');
-		image = searchParams.get('image');
-		sendMessage(token, text, image);
-	} else if (req.method === 'POST') {
-		let body = [];
-		req.on('data', (chunk) => {
-			body.push(chunk);
-		}).on('end', () => {
-			body = Buffer.concat(body).toString();
-			let contentType = req.headers['content-type'];
-			if (contentType && contentType.includes('application/json')) {
-				try {
-					let content = JSON.parse(body);
-					if (!token) token = content.token;
-					text = content.text;
-					image = content.image;
-				} catch (e) {
-					console.log(e);
-				}
-			} else if (contentType && contentType.includes('text/plain')) {
-				text = body;
-			}
-			sendMessage(token, text, image);
-		});
-	}
+/**
+ * 根据错误码获取响应内容
+ * @param {number} err 错误状态（0-成功; 1-token 失效; 2-不支持的请求方式; 3-服务器错误）
+ * @returns {string} 响应内容
+ */
+function getResponse(err) {
+	return `{err: ${err}, msg: "${responseText[err]}"}`;
+}
 
-	function sendMessage(token, text, image) {
-		let result = {
-			err: 0,
-			msg: 'success',
-		};
-		if (config.webhook.token.length && config.webhook.token !== token) {
-			result = {
-				err: 1,
-				msg: 'token',
-			};
-		} else {
-			sendRawMessage(text, image);
-			console.log(`${getTime()} webhook 调用成功`);
+server
+	.use(bodyParser.text({type: 'text/plain'}))
+	.use(bodyParser.json({type: 'application/json'}))
+	.use((err, _req, res, next) => {
+		res.type('json').end(getResponse(3));
+		next(err);
+	})
+	.get('/', (req, res) => {
+		const {token, text, image} = req.query;
+		if (ckeckToken(token)) {
+			res.type('json').end(getResponse(1));
+			return;
 		}
-
-		res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
-		res.end(`{err: ${result.err}, msg: "${result.msg}"}`);
-	}
-});
-
-server.listen(config.webhook.port, '0.0.0.0');
+		sendRawMessage(text, image);
+		res.type('json').end(getResponse(0));
+		console.log(`${getTime()} webhook 调用成功`);
+	})
+	.post('/', (req, res) => {
+		const type = req.get('Content-Type');
+		let token = req.query.token || '';
+		let image = '';
+		let text = '';
+		switch (type) {
+			case 'text/plain':
+				token = req.query.token;
+				text = req.body;
+				break;
+			case 'application/json':
+				token = token || req.body.token;
+				text = req.body.text;
+				image = req.body.image;
+				break;
+			default:
+				res.type('json').end(getResponse(2));
+				return;
+		}
+		if (ckeckToken(token)) {
+			res.type('json').end(getResponse(1));
+			return;
+		}
+		sendRawMessage(text, image);
+		res.type('json').end(getResponse(0));
+		console.log(`${getTime()} webhook 调用成功`);
+	})
+	.listen(config.webhook.port, '0.0.0.0');
